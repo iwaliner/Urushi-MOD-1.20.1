a202117aaddc72d3e2652b696f40bff92bfd9d9a
iwaliner <iwaliner.railway@gmail.com>
2024-12-27 11:58:15 +0900

    mandarin json

--- src/main/java/com/iwaliner/urushi/block/BearingMandarinLeavesBlock.java
+++ src/main/java/com/iwaliner/urushi/block/BearingMandarinLeavesBlock.java
@@ -1,10 +1,18 @@
 package com.iwaliner.urushi.block;
 
+import com.iwaliner.urushi.ItemAndBlockRegister;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,10 +20,11 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
 
-public class MandarinLeavesBlock extends LeavesBlock {
+public class BearingMandarinLeavesBlock extends LeavesBlock {
     public static final IntegerProperty AGE = BlockStateProperties.AGE_1;
-    public MandarinLeavesBlock(Properties p_54422_) {
+    public BearingMandarinLeavesBlock(Properties p_54422_) {
         super(p_54422_);
         this.registerDefaultState(this.stateDefinition.any().setValue(AGE, Integer.valueOf(0)));
     }
@@ -28,6 +37,21 @@ public class MandarinLeavesBlock extends LeavesBlock {
             net.minecraftforge.common.ForgeHooks.onCropsGrowPost(p_222564_, p_222565_, p_222563_);
         }
 
+    }
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        int i = state.getValue(AGE);
+        boolean flag = i == 1;
+       if (i ==1) {
+            int j = 1 + level.random.nextInt(2);
+            popResource(level, pos.above(), new ItemStack(ItemAndBlockRegister.manderin.get(), j ));
+            level.playSound((Player)null, pos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundSource.BLOCKS, 1.0F, 0.8F + level.random.nextFloat() * 0.4F);
+            BlockState blockstate = state.setValue(AGE, Integer.valueOf(0));
+            level.setBlock(pos, blockstate, 2);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, blockstate));
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        } else {
+            return super.use(state, level, pos, player, hand, result);
+        }
     }
 
     @Override
