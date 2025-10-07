76328bfc1a56b6e57c49a4cdcea8c4c72d6c1d87
iwaliner <iwaliner.railway@gmail.com>
2025-10-07 12:33:55 +0900

    tatami slab, tatami carpet, ore gen, bag gen

--- src/main/java/com/iwaliner/urushi/block/TatamiCarpetBlock.java
+++ src/main/java/com/iwaliner/urushi/block/TatamiCarpetBlock.java
@@ -1,35 +1,29 @@
 package com.iwaliner.urushi.block;
 
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.tags.FluidTags;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.SlabType;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.pathfinder.PathComputationType;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
 
-public class TatamiSlabBlock extends HorizonalRotateSlabBlock {
+public class TatamiCarpetBlock extends TwoDirectionShapedBlock{
     public static final BooleanProperty FLIP = BooleanProperty.create("flip");
-    public TatamiSlabBlock(Properties p_i48377_1_) {
-        super(p_i48377_1_);
-        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH).setValue(TYPE, SlabType.BOTTOM).setValue(WATERLOGGED, Boolean.valueOf(false)).setValue(FLIP,false));
+    public TatamiCarpetBlock(double d1, double d2, double d3, double d4, double d5, double d6, boolean canSurvive, Properties p_i48377_1_) {
+        super(d1, d2, d3, d4, d5, d6,canSurvive, p_i48377_1_);
+    }
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_49915_) {
+        p_49915_.add(FACING,FLIP);
     }
     @Override
     public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
@@ -40,18 +34,11 @@ public class TatamiSlabBlock extends HorizonalRotateSlabBlock {
     public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
         return 60;
     }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_49915_) {
-        p_49915_.add(FACING,TYPE,WATERLOGGED,FLIP);
-    }
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         Level level=context.getLevel();
         BlockPos pos=context.getClickedPos();
-        FluidState fluidstate = context.getLevel().getFluidState(pos);
-        BlockState blockstate1 = this.defaultBlockState().setValue(TYPE, SlabType.BOTTOM).setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER)).setValue(FACING,context.getHorizontalDirection().getOpposite());
-        BlockState state= context.getClickedFace() != Direction.DOWN && (context.getClickedFace() == Direction.UP || !(context.getClickLocation().y - (double)pos.getY() > 0.5D)) ? blockstate1 : blockstate1.setValue(TYPE, SlabType.TOP);
+       BlockState state= this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
         boolean flag=false;
         Direction direction=state.getValue(FACING);
         /*if(direction==Direction.NORTH||direction==Direction.EAST||direction==Direction.UP){
@@ -60,17 +47,17 @@ public class TatamiSlabBlock extends HorizonalRotateSlabBlock {
         if(direction.getAxis()== Direction.Axis.X){
             BlockState eastState=level.getBlockState(pos.east());
             BlockState westState=level.getBlockState(pos.west());
-            if(eastState.getBlock() instanceof TatamiSlabBlock&&eastState.getValue(FACING).getAxis()== Direction.Axis.X){
+            if(eastState.getBlock() instanceof TatamiCarpetBlock&&eastState.getValue(FACING).getAxis()== Direction.Axis.X){
                 flag=!eastState.getValue(FLIP);
-            }else if(westState.getBlock() instanceof TatamiSlabBlock&&westState.getValue(FACING).getAxis()== Direction.Axis.X){
+            }else if(westState.getBlock() instanceof TatamiCarpetBlock&&westState.getValue(FACING).getAxis()== Direction.Axis.X){
                 flag=!westState.getValue(FLIP);
             }
         }else if(direction.getAxis()== Direction.Axis.Z){
             BlockState northState=level.getBlockState(pos.north());
             BlockState southState=level.getBlockState(pos.south());
-            if(northState.getBlock() instanceof TatamiSlabBlock&&northState.getValue(FACING).getAxis()== Direction.Axis.Z){
+            if(northState.getBlock() instanceof TatamiCarpetBlock&&northState.getValue(FACING).getAxis()== Direction.Axis.Z){
                 flag=!northState.getValue(FLIP);
-            }else if(southState.getBlock() instanceof TatamiSlabBlock&&southState.getValue(FACING).getAxis()== Direction.Axis.Z){
+            }else if(southState.getBlock() instanceof TatamiCarpetBlock&&southState.getValue(FACING).getAxis()== Direction.Axis.Z){
                 flag=!southState.getValue(FLIP);
             }
         }
@@ -83,24 +70,21 @@ public class TatamiSlabBlock extends HorizonalRotateSlabBlock {
     }
     @Override
     public BlockState updateShape(BlockState state, Direction facing, BlockState state2, LevelAccessor level, BlockPos pos, BlockPos pos2) {
-        if (state.getBlock() instanceof TatamiSlabBlock) {
-            if (state.getValue(WATERLOGGED)) {
-                level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
-            }
-            BlockState finalState = super.updateShape(state, facing, state2, level, pos, pos2);
-            boolean flip=finalState.getValue(FLIP);
+        BlockState finalState = super.updateShape(state, facing, state2, level, pos, pos2);
+        if (finalState.getBlock() instanceof TatamiCarpetBlock) {
+             boolean flip=finalState.getValue(FLIP);
             if(finalState.getValue(FACING).getAxis()== Direction.Axis.X){
                 BlockState eastState=level.getBlockState(pos.east());
                 BlockState westState=level.getBlockState(pos.west());
-                if(eastState.getBlock() instanceof TatamiSlabBlock&&eastState.getValue(FACING).getAxis()== Direction.Axis.X){
+                if(eastState.getBlock() instanceof TatamiCarpetBlock&&eastState.getValue(FACING).getAxis()== Direction.Axis.X){
                     if(flip==eastState.getValue(FLIP)){
                         finalState= finalState.setValue(FLIP,!flip);
                     }
-                }else if(westState.getBlock() instanceof TatamiSlabBlock&&westState.getValue(FACING).getAxis()== Direction.Axis.X){
+                }else if(westState.getBlock() instanceof TatamiCarpetBlock&&westState.getValue(FACING).getAxis()== Direction.Axis.X){
                     if(flip==westState.getValue(FLIP)){
                         finalState= finalState.setValue(FLIP,!flip);
                     }
-                }else if(((!(eastState.getBlock() instanceof TatamiSlabBlock)||eastState.getBlock()instanceof TatamiSlabBlock&&eastState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&finalState.getValue(FLIP))||((!(westState.getBlock() instanceof TatamiSlabBlock)||westState.getBlock()instanceof TatamiSlabBlock&&westState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&!finalState.getValue(FLIP))){
+                }else if(((!(eastState.getBlock() instanceof TatamiCarpetBlock)||eastState.getBlock()instanceof TatamiCarpetBlock&&eastState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&finalState.getValue(FLIP))||((!(westState.getBlock() instanceof TatamiCarpetBlock)||westState.getBlock()instanceof TatamiCarpetBlock&&westState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&!finalState.getValue(FLIP))){
                     finalState=finalState.setValue(FLIP,!finalState.getValue(FLIP));
                     /*if(finalState.getValue(FACING)==Direction.NORTH||finalState.getValue(FACING)==Direction.EAST||finalState.getValue(FACING)==Direction.UP){
                         finalState=finalState.setValue(FLIP,!finalState.getValue(FLIP));
@@ -109,15 +93,15 @@ public class TatamiSlabBlock extends HorizonalRotateSlabBlock {
             }else if(finalState.getValue(FACING).getAxis()== Direction.Axis.Z){
                 BlockState northState=level.getBlockState(pos.north());
                 BlockState southState=level.getBlockState(pos.south());
-                if(northState.getBlock() instanceof TatamiSlabBlock&&northState.getValue(FACING).getAxis()== Direction.Axis.Z){
+                if(northState.getBlock() instanceof TatamiCarpetBlock&&northState.getValue(FACING).getAxis()== Direction.Axis.Z){
                     if(flip==northState.getValue(FLIP)){
                         finalState= finalState.setValue(FLIP,!flip);
                     }
-                }else if(southState.getBlock() instanceof TatamiSlabBlock&&southState.getValue(FACING).getAxis()== Direction.Axis.Z){
+                }else if(southState.getBlock() instanceof TatamiCarpetBlock&&southState.getValue(FACING).getAxis()== Direction.Axis.Z){
                     if(flip==southState.getValue(FLIP)){
                         finalState= finalState.setValue(FLIP,!flip);
                     }
-                }else if(((!(northState.getBlock() instanceof TatamiSlabBlock)||northState.getBlock()instanceof TatamiSlabBlock&&northState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&finalState.getValue(FLIP))||((!(southState.getBlock() instanceof TatamiSlabBlock)||southState.getBlock()instanceof TatamiSlabBlock&&southState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&!finalState.getValue(FLIP))){
+                }else if(((!(northState.getBlock() instanceof TatamiCarpetBlock)||northState.getBlock()instanceof TatamiCarpetBlock&&northState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&finalState.getValue(FLIP))||((!(southState.getBlock() instanceof TatamiCarpetBlock)||southState.getBlock()instanceof TatamiCarpetBlock&&southState.getValue(FACING).getAxis()!=finalState.getValue(FACING).getAxis())&&!finalState.getValue(FLIP))){
                     finalState=finalState.setValue(FLIP,!finalState.getValue(FLIP));
                     /*if(finalState.getValue(FACING)==Direction.NORTH||finalState.getValue(FACING)==Direction.EAST||finalState.getValue(FACING)==Direction.UP){
                         finalState=finalState.setValue(FLIP,!finalState.getValue(FLIP));
@@ -133,7 +117,4 @@ public class TatamiSlabBlock extends HorizonalRotateSlabBlock {
 
         return super.updateShape(state, facing, state2, level, pos, pos2);
     }
-
-
-
 }
--- src/main/java/com/iwaliner/urushi/mixin/PlayerMixin.java
+++ src/main/java/com/iwaliner/urushi/mixin/PlayerMixin.java
@@ -15,10 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Player.class)
 public class PlayerMixin {
 
-    @Inject(method = "tick",at = @At("HEAD"), cancellable = true)
+    /*@Inject(method = "tick",at = @At("HEAD"), cancellable = true)
     private void tick(CallbackInfo ci){
         if(!((Player) (Object)this).isCreative()&&((Player) (Object)this).getAbilities().mayfly&&!((Player) (Object)this).getInventory().contains(new ItemStack(ItemAndBlockRegister.open_wagasa.get()))){
            ((Player) (Object)this).getAbilities().mayfly=false;
         }
-    }
+    }*/
 }
